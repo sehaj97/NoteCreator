c02730e83adcc09a52295035158ab4768879132b
sehaj97 <sehajmagan@gmail.com>
2021-12-18 17:49:01 -0500

    added deklete functionality

--- routes/apiRoutes/notesRoutes/notes.js
+++ routes/apiRoutes/notesRoutes/notes.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
-const db = require('../../../db/db.json');
-const {validateNote, createNewNote} = require('../../../lib/notes');
+var uniqid = require('uniqid');
+const path = require('path');
+const fs = require('fs');
+const {validateNote, createNewNote, removeNote} = require('../../../lib/notes');
 
 router.get('/notes', (req, res) => {
-  let results = db;  
+  let results = fs.readFileSync(path.join(__dirname, '../../../db/db.json'));  
+  results = JSON.parse(results)
   if (results) {
     res.json(results);
   } else {
@@ -13,14 +16,23 @@ router.get('/notes', (req, res) => {
 
 
 router.post('/notes', (req, res) => {
+  req.body.id = uniqid();
     if (!validateNote(req.body)) {
         res.status(400).send('The note is not properly formatted.');
     } else {
-        const note = createNewNote(req.body, db);
+        const note = createNewNote(req.body);
         res.json(note);
     }
 });
 
 
+router.delete('/notes/:id', (req, res) => {
+  if (req.params.id) {
+    removeNote(req.params.id);
+    res.send('deleted!');
+  } else {
+    res.send(404);
+  }
+});
 
 module.exports = router;
