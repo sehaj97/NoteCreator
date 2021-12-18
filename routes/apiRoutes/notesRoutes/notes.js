76ca090afeee752fc0296458185cff7369667f01
sehaj97 <sehajmagan@gmail.com>
2021-12-18 16:31:21 -0500

    added functionality to handle pst rewquest

--- routes/apiRoutes/notesRoutes/notes.js
+++ routes/apiRoutes/notesRoutes/notes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const db = require('../../../db/db.json');
+const {validateNote, createNewNote} = require('../../../lib/notes');
 
 router.get('/notes', (req, res) => {
   let results = db;  
@@ -9,5 +10,17 @@ router.get('/notes', (req, res) => {
     res.send(404);
   }
 });
+
+
+router.post('/notes', (req, res) => {
+    if (!validateNote(req.body)) {
+        res.status(400).send('The note is not properly formatted.');
+    } else {
+        const note = createNewNote(req.body, db);
+        res.json(note);
+    }
+});
+
+
 
 module.exports = router;
