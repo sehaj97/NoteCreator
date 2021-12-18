143fc768da642f0abb8ad622f86ba55e5c029a8f
sehaj97 <sehajmagan@gmail.com>
2021-12-18 15:06:59 -0500

    added html routes

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 
+const htmlRoutes = require('./routes/htmlRoutes');
+
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+app.use('/', htmlRoutes);
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
